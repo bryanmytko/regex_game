338d01555dd3f61a6148282649829f90d3b860d8
Bryan Mytko <bryanmytko@gmail.com>
2016-04-16 12:56:47 -0400

    Add Mongo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 var mustacheExpress = require('mustache-express');
 var app = express();
 
@@ -12,5 +13,12 @@ app.get('/', function(req, res) {
 });
 
 app.listen(5000, function () {
-    console.log('Listening on port 3000!');
+    console.log('Listening on port 5000!');
+});
+
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
 });
