b9519c4a7fd3c86b118d114251cd4c96b9a9b403
Bryan Mytko <bryanmytko@gmail.com>
2016-04-16 18:56:25 -0400

    Extract db logic to db.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-var require('dotenv').config();
+require('dotenv').config();
 
 var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
 var mustacheExpress = require('mustache-express');
 var app = express();
+var db = require('./db');
 
 app.use(express.static('public'));
 app.engine('mustache', mustacheExpress());
@@ -12,17 +11,22 @@ app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
+  var collection = db.get().collection('scorecollection');
+  high_scores = collection.find().limit(2);
+
+  console.log(typeof(high_scores));
+
   res.render('index');
 });
 
-app.listen(5000, function () {
-    console.log('Listening on port 5000!');
+db.connect(function(err){
+  if(err){
+    console.log('Unable to connect to database.');
+    process.exit(1);
+  } else {
+    app.listen(5000, function () {
+      console.log('Listening on port 5000!');
+    });
+  }
 });
 
-/* Test Mongodb */
-var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
-});
